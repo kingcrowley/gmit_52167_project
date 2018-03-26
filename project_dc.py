a257cd30363e24445aea0bf3ed5fe291eed61f6e
kingcrowley <kingcrowley@users.noreply.github.com>
2018-03-26 15:36:13 +0100

    Moved count variable increment
    Moved count variable increment to end of loop. I'll use this to possibly to add the latest width, length to a list to calculate median and mode.

--- project_dc.py
+++ project_dc.py
@@ -15,13 +15,14 @@ count = 0
 with open('iris.data', 'rt') as irisdata:
     # for each line in the data set
     for line in irisdata:
-        count = count +1;
+        
         # print(line)
         # split the line and store it
         splitline = line.split(",")
         totalPetalLength += float(splitline[0])
         # print out details needed with the added string details to show the user the details needed
         print("Petal Length: " + splitline[0] + " " + "Petal Width: " + splitline[1] + " " + "Sepal Length: " + splitline[2] + " "+ "Sepal Width: " + splitline[3]) 
+        count = count +1
 # close the file       
 irisdata.close()
 print(count)
