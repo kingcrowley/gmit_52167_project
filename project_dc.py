205346289cf077345044e094154cb00b98ecda5c
kingcrowley <kingcrowley@users.noreply.github.com>
2018-03-26 11:06:56 +0100

    Started basic summary stats
    Added in variables to store totals for petal length, width and same for sepal
    
    used variable count to count the number of lines in dataset
    Stored a running total called totalPetalLength and divided it by count to get the avg/mean petal length

--- project_dc.py
+++ project_dc.py
@@ -5,14 +5,24 @@
 
 # open file iris.data in read mode (for text which is the default anyway)
 
+totalPetalLength = 0
+totalPetalWidth = 0
+totalSepalLength = 0
+totalSepalWidth = 0
 
+# to count the number of lines in the dataset
+count = 0
 with open('iris.data', 'rt') as irisdata:
     # for each line in the data set
     for line in irisdata:
+        count = count +1;
         # print(line)
         # split the line and store it
         splitline = line.split(",")
+        totalPetalLength += float(splitline[0])
         # print out details needed with the added string details to show the user the details needed
         print("Petal Length: " + splitline[0] + " " + "Petal Width: " + splitline[1] + " " + "Sepal Length: " + splitline[2] + " "+ "Sepal Width: " + splitline[3]) 
 # close the file       
 irisdata.close()
+print(count)
+print(totalPetalLength/count)
