8eb50991b3dcf73dc584b95c58251594f030715e
kingcrowley <kingcrowley@users.noreply.github.com>
2018-03-27 15:05:22 +0100

    Fixed mix-up of attributes in variable names
    Fixed my mix up of sepal length and petal length.
    
    Commented out a print that just showed count and added in a print to describe the stuff that is being printed out.

--- project_dc.py
+++ project_dc.py
@@ -6,7 +6,7 @@
 # open file iris.data in read mode (for text which is the default anyway)
 from statistics import median, mode, pstdev, mean
 from matplotlib import pyplot as plt
-petalLengthList = []
+sepalLengthList = []
 totalPetalLength = 0
 totalPetalWidth = 0
 totalSepalLength = 0
@@ -21,27 +21,30 @@ with open('iris.data', 'rt') as irisdata:
         # print(line)
         # split the line and store it
         splitline = line.split(",")
-        totalPetalLength += float(splitline[0])
+        totalSepalLength += float(splitline[0])
         # store latest petal length in the list for calculations.
-        petalLengthList.append(float(splitline[0]))
+        sepalLengthList.append(float(splitline[0]))
         # print out details needed with the added string details to show the user the details needed
-        print("Sepal Length: " + splitline[0] + " Sepal Width: " + splitline[1] + " Petal Length: " + splitline[2] + " Petal Width: " + splitline[3] + " Class: " + splitline[4]) 
+        # used for testing in this project. commented out
+        # print("Sepal Length: " + splitline[0] + " Sepal Width: " + splitline[1] + " Petal Length: " + splitline[2] + " Petal Width: " + splitline[3] + " Class: " + splitline[4]) 
         count = count +1
         
         
 # close the file       
 irisdata.close()
-print(count)
+# for testing
+# print(count)
 # using inbuilt python function to calcluate median etc. 
 # statistics link - https://docs.python.org/3/library/statistics.html
 # I could have used mean inbuilt into the statistics module to calculate the mean
-print("Mean from stats module: " + str(mean(petalLengthList)))
+print("Sepal Length - Descriptive Stats")
+print("Mean from stats module: " + str(mean(sepalLengthList)))
 # mean calculated by using total divided by count. Unnessecary with the stats module. 
 print("Mean: " + str(totalPetalLength/count))
-print("Median: " + str(median(petalLengthList)))
-print("Mode: " + str(mode(petalLengthList)))
-print("Standard Deviation: " + str(pstdev(petalLengthList)))
-print("Max: " + str(max(petalLengthList)))
-print("Min: " + str(min(petalLengthList)))
+print("Median: " + str(median(sepalLengthList)))
+print("Mode: " + str(mode(sepalLengthList)))
+print("Standard Deviation: " + str(pstdev(sepalLengthList)))
+print("Max: " + str(max(sepalLengthList)))
+print("Min: " + str(min(sepalLengthList)))
 
 
